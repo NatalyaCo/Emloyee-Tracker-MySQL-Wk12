aadd31ecb8e87cb8f4568a38c0343400ffd9c9b2
Natalya Colburn <greatqueenbee@gmail.com>
2022-05-18 10:51:48 -0600

    need to ask tutor which is correct seed format

--- db/seeds.sql
+++ db/seeds.sql
@@ -35,4 +35,12 @@ VALUES  ('Natalya', "C", 1),
         ("Jill", "Biden", 11);
 
 
+-- INSERT INTO department (name)
+-- VALUES ("Sales");
+-- INSERT INTO department (name)
+-- VALUES ("Engineering");
+-- INSERT INTO department (name)
+-- VALUES ("Finance");
+-- INSERT INTO department (name)
+-- VALUES ("Legal");
       
