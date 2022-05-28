b78e0dfc5b7c777af96497b4092abfa837327f92
Natalya Colburn <greatqueenbee@gmail.com>
2022-05-27 22:43:52 -0600

    going back to asynch functions

--- index.js
+++ index.js
@@ -96,3 +96,101 @@ break;
 
 initialAction();
 
+const viewEmployee = async () => {
+console.log('Employees');
+try {
+    let query = 'SELECT * FROM employee';
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        let employeeArray = [];
+        res.forEach(employee => employeeArray.push(employee));
+        console.table(employeeArray);
+initialAction();
+});
+} catch (err) {
+onsole.log(err);
+initialAction();
+};
+}
+
+const viewRoles = async () => {
+console.log('Roles');
+    
+try {
+let query = 'SELECT * FROM roles';
+    connection.query(query, function (err, res) {
+if (err) throw err;
+
+//does it have to be roles? or role?
+let roleArray = [];
+    res.forEach(role => roleArray.push(role));
+    console.table(roleArray);
+initialAction();
+});
+} catch (err) {
+    console.log(err);
+initialAction();
+};
+}
+
+const addEmployee = async () => {
+try {
+console.log('Adding Employee');
+
+let roles = await connection.query("SELECT * FROM roles");
+let managers = await connection.query("SELECT * FROM employee");
+let answer = await inquirer.prompt([
+    {
+    name: 'firstName',
+    type: 'input',
+    message: 'Enter the first namee of employee, please'
+    },
+    {
+    name: 'lastName',
+    type: 'input',
+    message: 'Enter the last name of employee, please'
+    },
+    {
+    name: 'employeeRoleId',
+    type: 'list',
+    choices: roles.map((role) => {
+    return {
+    name: role.title,
+    value: role.id
+    }
+        }),
+    message: "Enter employee's role id, please"
+    },
+    {
+    name: 'employeeManagerId',
+    type: 'list',
+    choices: managers.map((manager) => {
+    return {
+    name: manager.first_name + " " + manager.last_name,
+    value: manager.id
+    }
+        }),
+    message: "Enter employee's manager's id, please"
+    }
+        ])
+
+    let result = await connection.query("INSERT INTO employee SET ?", {
+    first_name: answer.firstName,
+    last_name: answer.lastName,
+    role_id: (answer.employeeRoleId),
+    manager_id: (answer.employeeManagerId)
+        });
+
+console.log(`${answer.firstName} ${answer.lastName} added successfully.\n`);
+        
+initialAction();
+
+} catch (err) {
+
+    console.log(err);
+        
+initialAction();
+};
+}
+
+
--- schema.sql
+++ schema.sql
@@ -16,22 +16,22 @@ id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL(10, 2),
 department_id INT NULL,
-FORGEIN KEY (department_id),
-REFERENCES (department_id),
-ON DELETE CASCASDE
+-- FORGEIN KEY (department_id),
+-- REFERENCES (department_id),
+-- ON DELETE CASCASDE
 );
 
-DROP TABLE IF EXISTS employee,
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 firstName VARCHAR(30) NOT NULL,
 lastName VARCHAR(30) NOT NULL,
 role_id INT,
 manager_id INT,
-FORGEIN KEY (roles_id),
-REFERENCES employee_roles(id),
-ON DELETE CASCASDE,
-FORGEIN KEY (manager_id),
-REFERENCES employee(id),
-ON DELETE CASCASDE
+-- FORGEIN KEY (roles_id),
+-- REFERENCES employee_roles(id),
+-- ON DELETE CASCASDE,
+-- FORGEIN KEY (manager_id),
+-- REFERENCES employee(id),
+-- ON DELETE CASCASDE
 );
